667f57bf760dd100f4ad9c80bb15c5fe112e6c7e
Liam Stanley <liamstanley1999@gmail.com>
2021-10-11 20:24:06 +1100

    darker foliage + other stuff

---
+++ GDS6Final/Assets/Scripts/MusicFadeBiome.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicFadeBiome : MonoBehaviour
+{
+    public AudioSource Music;
+
+    public float fadeFactor = 0.01f;
+
+    public bool fadeIn = false;
+    public bool fadeOut = false;
+
+    void Start()
+    {
+        Music.volume = 0.0f;
+    }
+
+
+    void Update()
+    {
+        if (fadeIn)
+        {
+            if (Music.volume <= 0.1)
+            {
+                Music.volume += fadeFactor * Time.deltaTime;
+                print("fadeIn");
+            }
+        }
+
+        if (fadeOut)
+        {
+            if (Music.volume >= 0)
+            {
+                Music.volume -= fadeFactor * Time.deltaTime;
+            }
+        }
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (Music.isPlaying == false){
+            Music.Play();
+        }
+       
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            fadeIn = true;
+            fadeOut = false;
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+
+            fadeOut = true;
+            fadeIn = false;
+            print("why");
+
+
+        }
+    }
+}
