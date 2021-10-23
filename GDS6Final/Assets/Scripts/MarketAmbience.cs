b4ba9a6c9b580877cb1ff95573558daed1cc1e81
Liam Stanley <liamstanley1999@gmail.com>
2021-10-23 19:19:16 +1100

    Animated meshes in (minus Pluvia Standing).

---
+++ GDS6Final/Assets/Scripts/MarketAmbience.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketAmbience : MonoBehaviour
+{
+    public AudioSource marketsound;
+    public float fadeFactor;
+
+    public bool fadeIn = false;
+    public bool fadeOut = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeOut)
+        {
+            if(marketsound.volume > 0.01)
+            {
+                marketsound.volume -= fadeFactor * Time.deltaTime;
+            }
+            
+        }
+
+        
+        if (fadeIn)
+        {
+            if (marketsound.volume <= 0.05)
+            {
+                marketsound.volume += fadeFactor * Time.deltaTime;
+            }
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            fadeIn = true;
+            fadeOut = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            fadeOut = true;
+            fadeIn = false;
+        }
+    }
+}
